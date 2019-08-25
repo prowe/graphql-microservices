2e3a438739a2d1fdb62052fd0006a37e14f9d822
Paul Rowe <prowe@sourceallies.com>
2019-08-24 19:57:49 -0500

    wip

--- graphql-api/deploy.sh
+++ graphql-api/deploy.sh
@@ -5,9 +5,9 @@ export AWS_REGION=us-east-1
 aws cloudformation package \
     --template-file=cloudformation.template.yml \
     --s3-bucket=prowe-deploy-bucket \
-    --output-template-file=cloudformation.transformed.yaml
+    --output-template-file=cloudformation.transformed.yml
 
 aws cloudformation deploy \
     --stack-name=prowe-graphql-api \
-    --template-file=cloudformation.transformed.yaml \
+    --template-file=ccloudformation.transformed.yml \
     --capabilities=CAPABILITY_IAM
